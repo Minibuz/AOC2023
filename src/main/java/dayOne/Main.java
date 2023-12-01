54d50854b1f7896da98a92302526a8bca9435069
Léo Buzelin <leo.buzelin@carbon-it.com>
2023-12-01 11:11:43 +0100

    feat: day 1 - part 1 and 2

--- src/main/java/dayOne/Main.java
+++ src/main/java/dayOne/Main.java
@@ -2,11 +2,88 @@ package dayOne;
 
 import utils.FileReader;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
+
 public class Main {
 
     public static void main(String[] args) {
-        var result = FileReader.readFile("test");
+        var lines = FileReader.readFile("day1.txt");
 
-        // Do something here
+        var part1 = lines.stream()
+                .map(l -> l.replaceAll("[a-zA-Z]", ""))
+                .map(l -> {
+                    var length = l.length();
+                    return switch (length) {
+                        case 0 -> "0";
+                        case 1 -> l + l;
+                        default -> l.charAt(0) + "" + l.charAt(length - 1);
+                    };
+                })
+                .mapToInt(Integer::parseInt)
+                .sum();
+        System.out.println(part1);
+
+//        Tentative Partie 2
+
+//        var pattern = Pattern.compile("(one|two|three|four|five|six|seven|eight|nine|[1-9])");
+//
+//        List<Integer> values = new ArrayList<>();
+//        for(var line : lines) {
+//
+//            var matcher = pattern.matcher(line);
+//            String first = null;
+//            String last = "";
+//            while (matcher.find()) {
+//                if (first == null) {
+//                    first = Main.transform(matcher.group(0));
+//                }
+//                last = Main.transform(matcher.group(0));
+//            }
+//            var value = first + last;
+//            values.add(Integer.parseInt(value));
+//        }
+//
+//        System.out.println(values.stream().mapToInt(Integer::intValue).sum());
+
+        var part2 = lines.stream()
+                .map(l -> l.replaceAll("one", "one1one")
+                        .replaceAll("two", "two2two")
+                        .replaceAll("three", "three3three")
+                        .replaceAll("four", "four4four")
+                        .replaceAll("five", "five5five")
+                        .replaceAll("six", "six6six")
+                        .replaceAll("seven", "seven7seven")
+                        .replaceAll("eight", "eight8eight")
+                        .replaceAll("nine", "nine9nine"))
+                .map(l -> l.replaceAll("[a-zA-Z]", ""))
+                .map(l -> {
+                    var length = l.length();
+                    return switch (length) {
+                        case 0 -> "0";
+                        case 1 -> l + l;
+                        default -> l.charAt(0) + "" + l.charAt(length - 1);
+                    };
+                })
+                .peek(System.out::println)
+                .mapToInt(Integer::parseInt)
+                .sum();
+        System.out.println(part2);
     }
+
+//    private static String transform(String value) {
+//        return switch (value) {
+//            case "one" -> "1";
+//            case "two" -> "2";
+//            case "three" -> "3";
+//            case "four" -> "4";
+//            case "five" -> "5";
+//            case "six" -> "6";
+//            case "seven" -> "7";
+//            case "eight" -> "8";
+//            case "nine" -> "9";
+//            default -> value;
+//        };
+//    }
 }
